63490e4f9c2253111ddca1a2b448b5cc96ffde24
Cody Burley <codyburley@gmail.com>
2022-11-22 14:33:19 -0500

    added date

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,19 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns/sub";
 
 const initialState = [
-  { id: '1', title: 'Test Title 1', content: 'Some content 1' },
-  { id: '2', title: 'Test Title 2', content: 'Some content 2' }
+  {
+    id: '1',
+    title: 'Test Title 1',
+    content: 'Some content 1',
+    date: sub(new Date(), { minutes: 10 }).toISOString()
+  },
+  {
+    id: '2',
+    title: 'Test Title 2',
+    content: 'Some content 2',
+    date: sub(new Date(), { minutes: 5 }).toISOString()
+  }
 ]
 
 const postsSlice = createSlice({
@@ -19,6 +30,7 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId
           }
         }
